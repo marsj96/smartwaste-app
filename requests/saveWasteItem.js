499de27b7ce41b541c0a0022324ca7682cd7f6ab
marsj96 <jack.oliver.marsden@gmail.com>
2023-02-06 15:31:35 +0000

    created post request for API

--- requests/saveWasteItem.js
+++ requests/saveWasteItem.js
@@ -1,16 +1,31 @@
 const axios = require('axios')
+const FormData = require('form-data');
+require('dotenv').config()
 
-function saveWasteItem() {
+let cid = process.env.CID
+let pid = process.env.PID
+let authToken = process.env.AUTHTOKEN
+let username = process.env.USERN
+
+function saveWasteItem(body) {
+
+    let formData = new FormData()
+    formData.append("wasteItem", JSON.stringify(body))
 
 axios({
     method: "post",
-    url: `https://api.smartwaste.co.uk/v1/${cid}/projects/${volumePid}/waste-items`,
-    data: bodyFormData,
-    headers: { "Content-Type": "multipart/form-data" },
+    url: `https://api.smartwaste.co.uk/v1/${cid}/projects/${pid}/waste-items`,
+    data: formData,
+    headers: { 
+                "Content-Type": "multipart/form-data",
+                "Accept" : "application/json",
+                authToken: authToken,
+                username: username
+            },
   })
     .then(function (response) {
       //handle success
-      console.log(response);
+      console.log(response.data);
     })
     .catch(function (response) {
       //handle error
@@ -18,3 +33,5 @@ axios({
     });
 
 };
+
+module.exports = { saveWasteItem }
