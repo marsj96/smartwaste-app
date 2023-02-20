7c40840758374fb8266527abbf099a85b9175309
marsj96 <jack.oliver.marsden@gmail.com>
2023-02-20 15:20:23 +0000

    reformatted to match Google Sheet layout

--- utils/createSaveWasteItem.js
+++ utils/createSaveWasteItem.js
@@ -1,10 +1,23 @@
 function createSaveWasteItem(dataItem) {
 
     console.log(dataItem)
+
+    let date 
+        =dataItem[1][8]
+        +dataItem[1][9]
+        +"/"
+        +dataItem[1][5]
+        +dataItem[1][6]
+        +"/"
+        +dataItem[1][0] 
+        + dataItem[1][1] 
+        + dataItem[1][2] 
+        + dataItem[1][3]
+        
     
     let body = {
         "wasteID": "",
-        "dateEntered": dataItem[1],
+        "dateEntered": date,
         "wasteCarrier": {
             "carrierID": 169540,
             "carrierName": "Sample waste carrier 1"
@@ -14,53 +27,53 @@ function createSaveWasteItem(dataItem) {
             "destinationName": "Sample destination 1"
         },
         "projectTransport": {
-            "totalDistanceKms": Number(dataItem[10]),
-            "totalDistanceMiles": (Math.round((dataItem[10]*0.62137119)*100))/100,
+            "totalDistanceKms": Number(dataItem[21]),
+            "totalDistanceMiles": Number(dataItem[22]),
             "modeOfTransport": {
-                "modeName": dataItem[12],
-                "modeID": Number(dataItem[13])
+                "modeName": "Road",
+                "modeID": 1
             },
             "vehicleType": {
-                "vehicleTypeName": dataItem[15],
-                "vehicleTypeID": Number(dataItem[16])
+                "vehicleTypeName": dataItem[10],
+                "vehicleTypeID": Number(dataItem[24])
             },
             "fuelType": {
-                "fuelTypeName": dataItem[17],
-                "fuelTypeID": dataItem != "" ? Number(dataItem[18]) : ""
+                "fuelTypeName": dataItem[11],
+                "fuelTypeID": dataItem != "" ? Number(dataItem[26]) : ""
             },
-            "fuelAmountInLitre": Number(dataItem[19])
+            "fuelAmountInLitre": Number(dataItem[27])
         },
         "wasteTransferNote": "z",
         "containerReference": "",
         "skipSize": {
-                 "description": dataItem[20], 
-                                "skipSizeID": Number(dataItem[21]),                                                      
-                                 "volume": Number(dataItem[22])
+                 "description": dataItem[12], 
+                                "skipSizeID": Number(dataItem[28]),                                                      
+                                 "volume": Number(dataItem[29])
                  },
-        "voidPercentage": Number(dataItem[23].replace("%", "")),
-        "numberOfContainers": Number(dataItem[24]),
-        "overallTonnage": Number(dataItem[25]),
-        "sicCode": dataItem[26],
+        "voidPercentage": 40,
+        "numberOfContainers": 1,
+        "overallTonnage": Number(dataItem[4]),
+        "sicCode": 42.11,
         "projectPhase": {
             "projectPhaseID": 3,
             "projectPhaseName": "Construction"
         },
         "wasteManagementRoute": {
-            "wasteManagementRouteID": Number(dataItem[28]),
-            "wasteManagementRouteName": dataItem[27]
+            "wasteManagementRouteID": 1,
+            "wasteManagementRouteName": "Reuse"
         },
         "wasteManagementLocation": {
-            "wasteManagementLocationID": Number(dataItem[30]),
-            "wasteManagementLocationName": dataItem[29]
+            "wasteManagementLocationID": 2,
+            "wasteManagementLocationName": "Off-site"
         },
-        "containerSegregated": dataItem[31],
+        "containerSegregated": "TRUE",
         "createdByCompanyName": "Multevo",
         "wasteTransferNoteEvidence": "",
         "wasteProducts": [
             {
-                "wasteProductID": Number(dataItem[4]),
-                "description": dataItem[5],
-                "ewcCode": dataItem[3],
+                "wasteProductID": Number(dataItem[17]),
+                "description": dataItem[18],
+                "ewcCode": dataItem[16],
                 "percentage": 100
             }
         ]
